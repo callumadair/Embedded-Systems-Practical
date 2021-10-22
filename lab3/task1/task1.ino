db25c63ce9260bbcbe1999047f46928b2813953a
Cal <callum.adair@outlook.com>
2021-10-22 13:37:24 +0100

    Update task1.ino

--- lab3/task1/task1.ino
+++ lab3/task1/task1.ino
@@ -1,9 +1,10 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(18, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(18, HIGH);
+  digitalWrite(18, LOW);
 }
