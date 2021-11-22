c38ccc59e33080566db48960248c8041025554e3
Cal <callum.adair@outlook.com>
2021-11-22 17:16:34 +0000

    setup and reset

--- lab5/task2/sw_driver.h
+++ lab5/task2/sw_driver.h
@@ -27,11 +27,17 @@ const uint32_t timg0_t0load_reg = 0x3FF5F020;
 // ------ IMPLEMENT THE FOLLOWING ----------------
 void setupTimer(uint16_t divval) {
     // Implement the timerSetup function using regWrite 
-    regWrite(divval, regRead(1) | 1 << 0)
+    regWrite(timg0_t0config_reg, regRead(31) | (1 << 31));
+    
+    regWrite(timg0_t0config_reg, regRead(30) | (1 << 30));
+
+    regWrite(timg0_t0config_reg, regRead(13) | (divval << 13));
 }
 
 void resetTimer() {
     // Implement the resetTimer function using regWrite
+    regWrite(timg0_t0config_reg, regRead(31) | (0 << 31));
+    regWrite(timg0_t0load_reg, regRead(0) | (0 << 0));
 }
 
 uint64_t readTimer() {
