8f053a177b1f2097050b33549cf0ce1f5bd6f3f7
Toby Rea <toby.e.rea@protonmail.com>
2021-12-01 10:46:30 +0000

    Formatting

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -30,26 +30,26 @@ void loop() {
   Serial.println(temp_in_c);
 
  json_str = "{
-    \"device\" : \"" + String(gid) + "\",
+    \"device\": \"" + String(gid) + "\",
     \"average\": \"19.4\",
-    \"values\":[
-       {\"timestamp\" : \"1134\", \"value\": \"19.3\"},
-       {\"timestamp\" : \"1134\", \"value\": \"19.3\"},
-       {\"timestamp\" : \"1034\", \"value\": \"19.4\"},
-       {\"timestamp\" : \"1234\", \"value\": \"19.4\"},
-       {\"timestamp\" : \"1334\", \"value\": \"19.4\"},
-       {\"timestamp\" : \"1434\", \"value\": \"19.5\"},
-       {\"timestamp\" : \"1534\", \"value\": \"19.4\"},
-       {\"timestamp\" : \"1634\", \"value\": \"19.2\"},
-       {\"timestamp\" : \"1734\", \"value\": \"19.5\"},
-       {\"timestamp\" : \"1834\", \"value\": \"19.4\"},
-       {\"timestamp\" : \"1934\", \"value\": \"19.2\"},
-       {\"timestamp\" : \"2034\", \"value\": \"19.5\"},
-       {\"timestamp\" : \"2134\", \"value\": \"19.4\"},
-       {\"timestamp\" : \"2234\", \"value\": \"19.2\"},
-       {\"timestamp\" : \"2334\", \"value\": \"19.5\"},
-       {\"timestamp\" : \"2434\", \"value\": \"19.4\"},
-       {\"timestamp\" : \"2534\", \"value\": \"19.2\"}
+    \"values\": [
+      {\"timestamp\": \"1134\", \"value\": \"19.3\"},
+      {\"timestamp\": \"1134\", \"value\": \"19.3\"},
+      {\"timestamp\": \"1034\", \"value\": \"19.4\"},
+      {\"timestamp\": \"1234\", \"value\": \"19.4\"},
+      {\"timestamp\": \"1334\", \"value\": \"19.4\"},
+      {\"timestamp\": \"1434\", \"value\": \"19.5\"},
+      {\"timestamp\": \"1534\", \"value\": \"19.4\"},
+      {\"timestamp\": \"1634\", \"value\": \"19.2\"},
+      {\"timestamp\": \"1734\", \"value\": \"19.5\"},
+      {\"timestamp\": \"1834\", \"value\": \"19.4\"},
+      {\"timestamp\": \"1934\", \"value\": \"19.2\"},
+      {\"timestamp\": \"2034\", \"value\": \"19.5\"},
+      {\"timestamp\": \"2134\", \"value\": \"19.4\"},
+      {\"timestamp\": \"2234\", \"value\": \"19.2\"},
+      {\"timestamp\": \"2334\", \"value\": \"19.5\"},
+      {\"timestamp\": \"2434\", \"value\": \"19.4\"},
+      {\"timestamp\": \"2534\", \"value\": \"19.2\"}
     ]
   }";
 
