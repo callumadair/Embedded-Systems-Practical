7258d2d789d30b9ceb316b1393a947efd42a85d0
ConnorH011 <connor.humphries1@aol.co.uk>
2021-12-01 20:56:50 +0000

    Update CW1.ino

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -5,11 +5,11 @@
 OneWire oneWire(26);
 DallasTemperature sensors(&oneWire);
 
-const char* ssid = "NETGEAR35";
-const char* password = "magicalfinch482";
-const char* server = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
+const char* ssid = "Galaxy S10+00ef";
+const char* password = "ConnorH01";
+const char* ws = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
 
-dotDevice ddev(ssid, password, server);
+dotDevice ddev(ssid, password, ws);
 
 const char* gid = "EEGhyIgq";
 
@@ -17,10 +17,16 @@ float temp_in_c;
 
 String json_str;
 
+dotDevice server_con(ssid, password, ws);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  ddev.connect()
+  ddev.connect();
+
+    json_str = {"device": "+String(gid)+", "average": 19.4, "values":[{"timestamp" : 1034, "value": 19.5}, {"timestamp" : 1134, "value": 19.4},{"timestamp" : 1234, "value": 19.2}]};
+
+   server_con.sendJSON(json_str); // where json_str is a String containing the command payload
 }
 
 void loop() {
@@ -28,30 +34,5 @@ void loop() {
   sensors.requestTemperatures();
   temp_in_c = sensors.getTempCByIndex(0);
   Serial.println(temp_in_c);
-
- json_str = "{
-    \"device\": \"" + String(gid) + "\",
-    \"average\": \"19.4\",
-    \"values\": [
-      {\"timestamp\": \"1134\", \"value\": \"19.3\"},
-      {\"timestamp\": \"1134\", \"value\": \"19.3\"},
-      {\"timestamp\": \"1034\", \"value\": \"19.4\"},
-      {\"timestamp\": \"1234\", \"value\": \"19.4\"},
-      {\"timestamp\": \"1334\", \"value\": \"19.4\"},
-      {\"timestamp\": \"1434\", \"value\": \"19.5\"},
-      {\"timestamp\": \"1534\", \"value\": \"19.4\"},
-      {\"timestamp\": \"1634\", \"value\": \"19.2\"},
-      {\"timestamp\": \"1734\", \"value\": \"19.5\"},
-      {\"timestamp\": \"1834\", \"value\": \"19.4\"},
-      {\"timestamp\": \"1934\", \"value\": \"19.2\"},
-      {\"timestamp\": \"2034\", \"value\": \"19.5\"},
-      {\"timestamp\": \"2134\", \"value\": \"19.4\"},
-      {\"timestamp\": \"2234\", \"value\": \"19.2\"},
-      {\"timestamp\": \"2334\", \"value\": \"19.5\"},
-      {\"timestamp\": \"2434\", \"value\": \"19.4\"},
-      {\"timestamp\": \"2534\", \"value\": \"19.2\"}
-    ]
-  }";
-
-   server_con.sendJSON(json_str); // where json_str is a String containing the command payload
+ 
 }
