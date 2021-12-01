61bc8845cc57d1fd722a14bf6f139b658f9f1d0c
Cal <callum.adair@outlook.com>
2021-12-01 22:36:33 +0000

    debugged successfully

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -28,16 +28,16 @@ String getTemperaturesJson(){
   String temp_json = "{ \"device\" : \""+String(gid)+"\", \"average\" : ";
   float sum = 0;
   
-  for(i = 0; i < 16; ++i) {
+  for(int i = 0; i < 16; ++i) {
     sensors.requestTemperatures();
     temps[i] = sensors.getTempCByIndex(0);
-    sum += temps[i]
+    sum += temps[i];
   }
   float average = sum / 16;
   temp_json += " \""+String(average)+"\",  \"values\" : [ ";
 
   for(int j = 0; j < 16; ++j) {
-    temp_json += "{\"timestamp\" : 100, \"value\" : \""+String(temps[j]+"\" },"
+    temp_json += "{\"timestamp\" : 100, \"value\" : \""+ String(temps[j])+"\" },";
   }
   temp_json += " ] }";
   return temp_json;
@@ -47,6 +47,6 @@ void loop() {
   // put your main code here, to run repeatedly:
 
  //json_str = "{ \"device\" : \""+String(gid)+"\", \"average\": 19.4, \"values\" : [ {\"timestamp\" : 1034, \"value\": 19.5}, {\"timestamp\" : 1134, \"value\": 19.4}, {\"timestamp\" : 1234, \"value\": 19.2}, {\"timestamp\" : 1334, \"value\": 19.4} ]}";
-   server_con.sendJSON(getTemperatures());
+   server_con.sendJSON(getTemperaturesJson());
    //server_con.sendJSON(json_str); // where json_str is a String containing the command payload
 }
