59f68cb40afbbfdc8eed9b6145c7d0ec9df5bf20
Cal <callum.adair@outlook.com>
2021-12-01 21:48:59 +0000

    moved temperature readings to separate function

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -9,7 +9,7 @@ const char* ssid = "Galaxy S10+00ef";
 const char* password = "ConnorH01";
 const char* ws = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
 
-dotDevice server_con(ssid, password, server);
+dotDevice server_con(ssid, password, ws);
 
 const char* gid = "EEGhyIgq";
 
@@ -17,21 +17,27 @@ float temp_in_c;
 
 String json_str;
 
-dotDevice server_con(ssid, password, ws);
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   server_con.connect();
 }
 
+float[] getTemperatures(){
+  float temps[16];
+  for(i = 0; i < 16; ++i) {
+    sensors.requestTemperatures();
+    temps[i] = temp_in_c = sensors.getTempCByIndex(0);
+    Serial.println(temps[i]);
+  }
+
+  return temps;
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  sensors.requestTemperatures();
-  temp_in_c = sensors.getTempCByIndex(0);
-  Serial.println(temp_in_c);
 
- json_str = "{ \"device\" : \""+String(gid)+"\", \"average\": 19.4, \"values\" : [ {\"timestamp\" : 1034, \"value\": 19.5}] }";
+ //json_str = "{ \"device\" : \""+String(gid)+"\", \"average\": 19.4, \"values\" : [ {\"timestamp\" : 1034, \"value\": 19.5}, {\"timestamp\" : 1134, \"value\": 19.4}, {\"timestamp\" : 1234, \"value\": 19.2}, {\"timestamp\" : 1334, \"value\": 19.4} }";
 
    server_con.sendJSON(json_str); // where json_str is a String containing the command payload
 }
