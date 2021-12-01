26c3dbb143ebf2d99a5cba06a580baefe18ca5c1
ConnorH011 <connor.humphries1@aol.co.uk>
2021-12-01 21:48:48 +0000

    Update CW1.ino

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -7,7 +7,7 @@ DallasTemperature sensors(&oneWire);
 
 const char* ssid = "Galaxy S10+00ef";
 const char* password = "ConnorH01";
-const char* ws = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
+const char* server = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
 
 dotDevice server_con(ssid, password, server);
 
@@ -16,8 +16,6 @@ const char* gid = "EEGhyIgq";
 float temp_in_c;
 
 String json_str;
-
-dotDevice server_con(ssid, password, ws);
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,6 +28,7 @@ void loop() {
   sensors.requestTemperatures();
   temp_in_c = sensors.getTempCByIndex(0);
   Serial.println(temp_in_c);
+  
 
  json_str = "{ \"device\" : \""+String(gid)+"\", \"average\": 19.4, \"values\" : [ {\"timestamp\" : 1034, \"value\": 19.5}] }";
 
