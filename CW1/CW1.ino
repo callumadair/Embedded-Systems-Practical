509cf3377f53fa2ed4c7effda1624426c63af2aa
ConnorH011 <connor.humphries1@aol.co.uk>
2021-12-01 10:16:01 +0000

    changes

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -1,27 +1,42 @@
+#include "OneWire.h"
+#include "DallasTemperature.h"
+#include "dotDevice.h"
+
 OneWire oneWire(26);
 DallasTemperature sensors(&oneWire);
 
+const char* ssid = "NETGEAR35";
+const char* password = "magicalfinch482";
+const char* server = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
+
+dotDevice ddev(ssid, password, server);
+
+const char* gid = "EEGhyIgq";
+
 float temp_in_c;
 
-dotDevice server_con(ssid, password, ws);
+String json_str;
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensors.requestTemperatures();
   temp_in_c = sensors.getTempCByIndex(0);
+  Serial.println(temp_in_c);
 
-  json_str = "{
-    "device": "wibble00",
+}
+ json_str = "{
+    \"device\" : \""+String(gid)+"\",
     "average": 19.4,
     "values":[ 
-       {"timestamp" : 1034, "value": 19.5},
-       {"timestamp" : 1134, "value": 19.4},
-       {"timestamp" : 1234, "value": 19.2},
+       {"timestamp" : 1034, "value": temp_in_c},
+       {"timestamp" : 1134, "value": temp_in_c},
+       {"timestamp" : 1234, "value": temp_in_c},
        {"timestamp" : 1334, "value": 19.4},
        {"timestamp" : 1434, "value": 19.5},
        {"timestamp" : 1534, "value": 19.4},
@@ -37,8 +52,7 @@ void loop() {
        {"timestamp" : 2534, "value": 19.2}
     ] 
   }"
-  
 
-  server_con.sendJSON(json_str); // where json_str is a String containing the command payload
+   server_con.sendJSON(json_str); // where json_str is a String containing the command payload
   
-}
+  
