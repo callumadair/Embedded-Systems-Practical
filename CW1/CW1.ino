e042782159bcf5d89db0d5a18b73ab71e86bce20
Cal <callum.adair@outlook.com>
2021-12-01 22:25:54 +0000

    removed json to temperature function

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -7,7 +7,7 @@ DallasTemperature sensors(&oneWire);
 
 const char* ssid = "Galaxy S10+00ef";
 const char* password = "ConnorH01";
-const char* server = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
+const char* ws = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
 
 dotDevice server_con(ssid, password, ws);
 
@@ -23,21 +23,30 @@ void setup() {
   server_con.connect();
 }
 
-float[] getTemperatures(){
+String getTemperaturesJson(){
   float temps[16];
+  String temp_json = "{ \"device\" : \""+String(gid)+"\", \"average\" : ";
+  float sum = 0;
+  
   for(i = 0; i < 16; ++i) {
     sensors.requestTemperatures();
-    temps[i] = temp_in_c = sensors.getTempCByIndex(0);
-    Serial.println(temps[i]);
+    temps[i] = sensors.getTempCByIndex(0);
+    sum += temps[i]
   }
+  float average = sum / 16;
+  temp_json += " \""+String(average)+"\",  \"values\" : [ ";
 
-  return temps;
+  for(int j = 0; j < 16; ++j) {
+    temp_json += "{\"timestamp\" : 100, \"value\" : \""+String(temps[j]+"\" },"
+  }
+  temp_json += " ] }";
+  return temp_json;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
- //json_str = "{ \"device\" : \""+String(gid)+"\", \"average\": 19.4, \"values\" : [ {\"timestamp\" : 1034, \"value\": 19.5}, {\"timestamp\" : 1134, \"value\": 19.4}, {\"timestamp\" : 1234, \"value\": 19.2}, {\"timestamp\" : 1334, \"value\": 19.4} }";
-
+ //json_str = "{ \"device\" : \""+String(gid)+"\", \"average\": 19.4, \"values\" : [ {\"timestamp\" : 1034, \"value\": 19.5}, {\"timestamp\" : 1134, \"value\": 19.4}, {\"timestamp\" : 1234, \"value\": 19.2}, {\"timestamp\" : 1334, \"value\": 19.4} ]}";
+   getTemperatures();
    server_con.sendJSON(json_str); // where json_str is a String containing the command payload
 }
