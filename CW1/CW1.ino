d0b3cdd23ab674e8a7d9141111eb17ce6508d440
Cal <callum.adair@outlook.com>
2021-12-01 22:27:21 +0000

    Update CW1.ino

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -9,7 +9,7 @@ const char* ssid = "Galaxy S10+00ef";
 const char* password = "ConnorH01";
 const char* ws = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
 
-dotDevice server_con(ssid, password, server);
+dotDevice server_con(ssid, password, ws);
 
 const char* gid = "EEGhyIgq";
 
@@ -21,10 +21,6 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   server_con.connect();
-
-   json_str = "{ \"device\" : \""+String(gid)+"\", \"average\": 19.4, \"values\" : [ {\"timestamp\" : 1034, \"value\": 19.5}] }";
-   server_con.sendJSON(json_str); // where json_str is a String containing the command payload
-
 }
 
 String getTemperaturesJson(){
@@ -51,6 +47,6 @@ void loop() {
   // put your main code here, to run repeatedly:
 
  //json_str = "{ \"device\" : \""+String(gid)+"\", \"average\": 19.4, \"values\" : [ {\"timestamp\" : 1034, \"value\": 19.5}, {\"timestamp\" : 1134, \"value\": 19.4}, {\"timestamp\" : 1234, \"value\": 19.2}, {\"timestamp\" : 1334, \"value\": 19.4} ]}";
-   getTemperatures();
-   server_con.sendJSON(json_str); // where json_str is a String containing the command payload
+   server_con.sendJSON(getTemperatures());
+   //server_con.sendJSON(json_str); // where json_str is a String containing the command payload
 }
