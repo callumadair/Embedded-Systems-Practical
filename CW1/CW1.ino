ae690662cbd165bb65febcdf88b690f4bfec4c64
Toby Rea <toby.e.rea@protonmail.com>
2021-12-03 16:42:57 +0000

    Organisation and comments
    Final code cleanup before branching to reimplement the payload in binary format

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -2,58 +2,61 @@
 #include "DallasTemperature.h"
 #include "dotDevice.h"
 
+// Setup the DallasTemperature library
 OneWire oneWire(26);
 DallasTemperature sensors(&oneWire);
 
+// Configuration
+const char* gid = "EEGhyIgq";
 const char* ssid = "Galaxy S10+00ef";
 const char* password = "ConnorH01";
 const char* ws = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
 
 dotDevice server_con(ssid, password, ws);
 
-const char* gid = "EEGhyIgq";
 float temp_in_c;
 String json_str;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   server_con.connect();
 }
 
-String getTemperaturesJson(){
+String getTemperaturesJson() {
+  
   float temps[16];
   unsigned long timestamps[16];
-  //initial time = 0
   unsigned long start = millis();
+
+  // Initialise the JSON payload
   String temp_json = "{ \"device\": \""+String(gid)+"\", \"average\": ";
-  
+
+  // Collect 16 temperature readings
   float sum = 0.0;
   for(int i = 0; i < 16; ++i) {
     sensors.requestTemperatures();
     temps[i] = sensors.getTempCByIndex(0);
-    //Serial.println(temps[i]);
     timestamps[i] = millis() - start;
-    //Serial.println(timestamps[i]);
     sum += temps[i];
   }
+  
+  // Calculate average temperature and finish JSON payload construction
   float average = sum / 16;
   temp_json += ""+String(average)+", \"values\": [ ";
-
   for(int j = 0; j < 15; ++j) {
-    
     temp_json += "{\"timestamp\" : "+String(timestamps[j])+", \"value\": "+String(temps[j])+"}, ";
   }
   temp_json += "{\"timestamp\" : "+String(timestamps[15])+", \"value\": "+String(temps[15])+"} ]}";
-  Serial.println(temp_json);
-
+  
+  Serial.println(temp_json); // FIXME: DEBUG PURPOSES ONLY
   return temp_json;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
    unsigned long start = millis();
    server_con.sendJSON(getTemperaturesJson());
+
+   // Delay ~30s between each payload
    unsigned long end = millis();
    delay(30000 - (end - start));
 }
