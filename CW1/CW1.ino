92bcd5e89b406964b84fd47f891c9fb6fa925499
Toby Rea <40074404+Toby-Rea@users.noreply.github.com>
2021-12-05 14:26:32 +0000

    Formatting changes

--- CW1/CW1.ino
+++ CW1/CW1.ino
@@ -10,10 +10,10 @@ DallasTemperature sensors(&oneWire);
 #pragma pack(1)
 struct data_packet_struct
 {
-  char gid[8];
-  uint16_t cmd;
-  uint16_t average;
-  uint16_t data[32];
+    char gid[8];
+    uint16_t cmd;
+    uint16_t average;
+    uint16_t data[32];
 } data_packet;
 #pragma pop(1)
 
@@ -26,36 +26,34 @@ const char* ws = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234";
 dotDevice server_con(ssid, password, ws);
 
 void setup() {
-  data_packet.cmd = 1; // Should always be "b0000000000000001"
-  // data_packet.gid = ["E", "E", "G", "h", "y", "I", "g", "q"]; // Not sure if necessary (handled by strcpy)
-  strcpy(data_packet.gid, gid); 
-  Serial.begin(115200);
-  server_con.connect();
+    data_packet.cmd = 1; // Should always be "b0000000000000001"
+    // data_packet.gid = ["E", "E", "G", "h", "y", "I", "g", "q"]; // Not sure if necessary (handled by strcpy)
+    strcpy(data_packet.gid, gid);
+    Serial.begin(115200);
+    server_con.connect();
 }
 
 void collectTemperatures(struct data_packet_struct *packet) {
-  // Log start time
-  unsigned long start = millis();
+    // Collect 16 temperatures and their respective timestamps
+    unsigned long start = millis();
+    unsigned long sum = 0;
+    for (int i = 0; i < 32; i + 2) {
+        sensors.requestTemperatures();
+        packet->data[i] = start + millis();
+        packet->data[i + 1] = sensors.getTempCByIndex(0);
+        sum += packet->data[i + 1];
+    };
 
-  // Collect 16 temperatures and their respective timestamps
-  unsigned long sum = 0;
-  for (int i = 0; i < 32; i + 2) {
-      sensors.requestTemperatures();
-      packet->data[i] = start + millis();
-      packet->data[i + 1] = sensors.getTempCByIndex(0);
-      sum += packet->data[i + 1];
-  };
-
-  // Calculate the mean average of the temperatures
-  packet->average = sum / 16;
+    // Calculate the mean average of the temperatures
+    packet->average = sum / 16;
 }
 
 void loop() {
-   unsigned long start = millis();
-   collectTemperatures(&data_packet);
-   server_con.sendBIN((char *)&data_packet, sizeof(data_packet));
+    unsigned long start = millis();
+    collectTemperatures(&data_packet);
+    server_con.sendBIN((char *)&data_packet, sizeof(data_packet));
 
-   // Delay ~30s between each payload
-   unsigned long end = millis();
-   delay(30000 - (end - start));
+    // Delay ~30s between each payload
+    unsigned long end = millis();
+    delay(30000 - (end - start));
 }
